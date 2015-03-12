22dbc44cbbe32e5d5602f77d72a612e178e11699
Taiki Fnit <taikifnitspg@gmail.com>
2015-03-12 10:58:30 +0900

    correspondence express4

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -21,14 +18,12 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
+app.use(cookieParser());
 app.use(session({
   secret: 'secret',
+  saveUninitialized: true,
   store: new MongoStore({
     db: 'session',
     host: 'localhost',
@@ -36,9 +31,17 @@ app.use(session({
   }),
   cookie: {
     httpOnly: false,
+    // bellow is a recommended option. however, it requires an https-enabled website...
+    // secure: true,
     maxAge: new Date(Date.now() + 60 * 60 * 1000)
   }
 }));
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -70,6 +73,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
